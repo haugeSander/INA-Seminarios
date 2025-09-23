be05316ea291d4aa66119cc799f3e97d52549e4d
haugeSander <hauge_Sander@outlook.com>
2025-09-23 11:27:08 +0200

    add: task 8

--- dispositivo/src/dispositivo/api/mqtt/Dispositivo_APIMQTT.java
+++ dispositivo/src/dispositivo/api/mqtt/Dispositivo_APIMQTT.java
@@ -1,5 +1,6 @@
 package dispositivo.api.mqtt;
 
+import java.util.Arrays;
 import java.util.UUID;
 
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken;
@@ -22,28 +23,27 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 	protected MqttClient myClient;
 	protected MqttConnectOptions connOpt;
 	protected String clientId = null;
-	
+
 	protected IDispositivo dispositivo;
 	protected String mqttBroker = null;
-	
+
 	private String loggerId = null;
-	
+
 	public static Dispositivo_APIMQTT build(IDispositivo dispositivo, String brokerURL) {
 		Dispositivo_APIMQTT api = new Dispositivo_APIMQTT(dispositivo);
 		api.setBroker(brokerURL);
 		return api;
 	}
-	
+
 	protected Dispositivo_APIMQTT(IDispositivo dev) {
 		this.dispositivo = dev;
 		this.loggerId = dev.getId() + "-apiMQTT";
 	}
-	
+
 	protected void setBroker(String mqttBrokerURL) {
 		this.mqttBroker = mqttBrokerURL;
 	}
-	
-	
+
 	@Override
 	public void connectionLost(Throwable t) {
 		MySimpleLogger.debug(this.loggerId, "Connection lost!");
@@ -52,7 +52,8 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 
 	@Override
 	public void deliveryComplete(IMqttDeliveryToken token) {
-		//System.out.println("Pub complete" + new String(token.getMessage().getPayload()));
+		// System.out.println("Pub complete" + new
+		// String(token.getMessage().getPayload()));
 	}
 
 	@Override
@@ -63,39 +64,55 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 		} catch (JSONException e) {
 			MySimpleLogger.warn(this.loggerId, "Mensaje no válido en topic " + topic);
 			return;
-
 		}
-		
+
 		System.out.println("-------------------------------------------------");
 		System.out.println("| Topic:" + topic);
 		System.out.println("| Message: " + payload);
 		System.out.println("-------------------------------------------------");
-				
+
+		String action = payload.getString("accion").trim();
+
 		String[] topicNiveles = topic.split("/");
-		String funcionId = topicNiveles[topicNiveles.length-2];
-		//String deviceId = topicNiveles[topicNiveles.length-4];
-		
-		IFuncion f = this.dispositivo.getFuncion(funcionId);
-		if ( f == null ) {
-			MySimpleLogger.warn(this.loggerId, "No encontrada funcion " + funcionId);
-			return;
+		if (topicNiveles[topicNiveles.length - 1].equalsIgnoreCase("comandos")) {
+			if (action == null || action.isBlank()) {
+				MySimpleLogger.warn(this.loggerId, "Mensaje sin acción en topic " + topic);
+				return;
+			}
 		}
 
-		//
-		// Definimos una API con mensajes de acciones básicos
-		//
-
-		// Ejecutamos acción indicada en campo 'accion' del JSON recibido
-		String action = payload.getString("accion");
-		
-		if ( action.equalsIgnoreCase("encender") )
-			f.encender();
-		else if ( action.equalsIgnoreCase("apagar") )
-			f.apagar();
-		else if ( action.equalsIgnoreCase("parpadear") )
-			f.parpadear();
-		else
-			MySimpleLogger.warn(this.loggerId, "Acción '" + payload + "' no reconocida. Sólo admitidas: encender, apagar o parpadear");
+		if (Arrays.stream(topicNiveles).anyMatch("funcion"::equals)) {
+			String funcionId = topicNiveles[topicNiveles.length - 2];
+			IFuncion f = this.dispositivo.getFuncion(funcionId);
+			if (f == null) {
+				MySimpleLogger.warn(this.loggerId, "No encontrada funcion " + funcionId);
+				return;
+			}
+			switch (action.toLowerCase()) {
+				case "encender":
+					f.encender();
+					break;
+				case "apagar":
+					f.apagar();
+					break;
+				case "parpadear":
+					f.parpadear();
+					break;
+				default:
+					MySimpleLogger.warn(this.loggerId, "Acción '" + action + "' no reconocida para función.");
+			}
+		} else {
+			switch (action.toLowerCase()) {
+				case "habilitar":
+					dispositivo.habilitar();
+					break;
+				case "deshabilitar":
+					dispositivo.deshabilitar();
+					break;
+				default:
+					MySimpleLogger.warn(this.loggerId, "Acción '" + action + "' no reconocida para dispositivo.");
+			}
+		}
 
 	}
 
@@ -110,12 +127,12 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 		// setup MQTT Client
 		String clientID = this.dispositivo.getId() + UUID.randomUUID().toString() + ".subscriber";
 		connOpt = new MqttConnectOptions();
-		
+
 		connOpt.setCleanSession(true);
 		connOpt.setKeepAliveInterval(30);
-//			connOpt.setUserName(M2MIO_USERNAME);
-//			connOpt.setPassword(M2MIO_PASSWORD_MD5.toCharArray());
-		
+		// connOpt.setUserName(M2MIO_USERNAME);
+		// connOpt.setPassword(M2MIO_PASSWORD_MD5.toCharArray());
+
 		// Connect to Broker
 		try {
 			MqttDefaultFilePersistence persistence = null;
@@ -123,7 +140,7 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 				persistence = new MqttDefaultFilePersistence("/tmp");
 			} catch (Exception e) {
 			}
-			if ( persistence != null )
+			if (persistence != null)
 				myClient = new MqttClient(this.mqttBroker, clientID, persistence);
 			else
 				myClient = new MqttClient(this.mqttBroker, clientID);
@@ -134,14 +151,13 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 			e.printStackTrace();
 			System.exit(-1);
 		}
-		
+
 		MySimpleLogger.info(this.loggerId, "Conectado al broker " + this.mqttBroker);
 
 	}
-	
-	
+
 	public void disconnect() {
-		
+
 		// disconnect
 		try {
 			// wait to ensure subscribed messages are delivered
@@ -152,12 +168,10 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 			e.printStackTrace();
 		}
 
-		
 	}
 
-	
 	protected void subscribe(String myTopic) {
-		
+
 		// subscribe to topic
 		try {
 			int subQoS = 0;
@@ -166,51 +180,45 @@ public class Dispositivo_APIMQTT implements MqttCallback {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
 
 	protected void unsubscribe(String myTopic) {
-		
+
 		// unsubscribe to topic
 		try {
 			myClient.unsubscribe(myTopic);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	public void iniciar() {
 
-		if ( this.myClient == null || !this.myClient.isConnected() )
+		if (this.myClient == null || !this.myClient.isConnected())
 			this.connect();
-		
-		if ( this.dispositivo == null )
+
+		if (this.dispositivo == null)
 			return;
-		
-		for(IFuncion f : this.dispositivo.getFunciones())
+
+		for (IFuncion f : this.dispositivo.getFunciones())
 			this.subscribe(this.calculateCommandTopic(f));
 
 	}
-	
-	
-	
+
 	public void detener() {
-		
-		
+
 		// To-Do
-		
+
 	}
-	
-	
+
 	protected String calculateCommandTopic(IFuncion f) {
 		return Configuracion.TOPIC_BASE + "dispositivo/" + dispositivo.getId() + "/funcion/" + f.getId() + "/comandos";
 	}
-	
+
 	protected String calculateInfoTopic(IFuncion f) {
 		return Configuracion.TOPIC_BASE + "dispositivo/" + dispositivo.getId() + "/funcion/" + f.getId() + "/info";
 	}
-	
 
 }
