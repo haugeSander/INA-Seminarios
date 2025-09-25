48896af688f7f2900aa9c0a75154bd092cdf2091
NicoleValeria1 <111001598+NicoleValeria1@users.noreply.github.com>
2025-09-25 16:33:23 +0200

    Ultimos cambios

--- setup.sh
+++ setup.sh
@@ -6,9 +6,9 @@ BROKER="tcp://localhost:1883"
 IP="localhost"
 
 # Start the two devices in the background
-java -jar seminari-2-vscode-alumno.jar sem1 $IP 8182 $BROKER &
+java -jar INA-Seminarios.jar sem1 $IP 8182 $BROKER &
 PID1=$!
-java -jar seminari-2-vscode-alumno.jar sem2 $IP 8183 $BROKER &
+java -jar INA-Seminarios.jar sem2 $IP 8183 $BROKER &
 PID2=$!
 
 # Give them time to connect
